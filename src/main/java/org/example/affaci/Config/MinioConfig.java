21866534e18f6573e3e06a85e41c9c78e71ad121
ElemanovBekzhan <94730786+ElemanovBekzhan@users.noreply.github.com>
2025-05-10 22:52:24 +0600

    fix MinIO

--- src/main/java/org/example/affaci/Config/MinioConfig.java
+++ src/main/java/org/example/affaci/Config/MinioConfig.java
@@ -35,7 +35,7 @@ public class MinioConfig {
     @Bean
     public MinioClient minioClient() {
         return MinioClient.builder()
-                .endpoint("affaci.com", 443, true)
+                .endpoint(minioUrl)
                 .credentials(accessKey, secretKey)
                 .region("us-east-1")
                 .build();
