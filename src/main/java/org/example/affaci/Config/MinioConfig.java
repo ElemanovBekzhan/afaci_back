333619fe51cb4af642e2231372cc5a6fe2a24797
ElemanovBekzhan <94730786+ElemanovBekzhan@users.noreply.github.com>
2025-05-11 21:43:31 +0600

    fix

--- src/main/java/org/example/affaci/Config/MinioConfig.java
+++ src/main/java/org/example/affaci/Config/MinioConfig.java
@@ -35,7 +35,7 @@ public class MinioConfig {
     @Bean
     public MinioClient minioClient() {
         return MinioClient.builder()
-                .endpoint("https://aafaci.com/minio/")
+                .endpoint("https://aafaci.com:9000")
                 .credentials(accessKey, secretKey)
                 .build();
     }
